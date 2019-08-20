aeaa2313235255e39eb6b5aa979a77b9808eef3e
Faming Hou <faming.hou@gmail.com>
2019-08-20 17:04:54 +1200

    Feature/add new components (#2)
    * import ElementUI
    
    * set language as en
    
    * install axios
    
    * add drawer with fake data
    
    * get top 10 posts when a user is selected;
    get details when a post is selected.

--- src/components/BlogManager.vue
+++ src/components/BlogManager.vue
@@ -1,114 +1,88 @@
 <template>
-  <div class="hello">
+  <div>
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
+
+    <el-table :data="users" border stripe @row-click="onUserSelected">
+      <el-table-column property="name" label="User Name" width="200"></el-table-column>
+      <el-table-column property="email" label="Email"></el-table-column>
+    </el-table>
+
+    <el-drawer title="I am the list of posts written by the selected user" :visible.sync="drawer" size="60%">
+      <div>
+        <el-table :data="postsWrittenBySelectedUser" border stripe @row-click="onPostSelected">
+          <el-table-column property="title" label="Title"></el-table-column>
+        </el-table>
+
+        <el-drawer
+          title="I am the details of one post"
+          :append-to-body="true"
+          :visible.sync="innerDrawer"
         >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+          <div>
+            <el-card v-if="selectedPost" class="box-card">
+              <div slot="header" class="clearfix">
+                <span>Id: {{selectedPost.id}}</span>
+                <el-divider></el-divider>
+                <span>User Id: {{selectedPost.userId}}</span>
+                <el-divider></el-divider>
+                <span>Title: {{selectedPost.title}}</span>
+                <el-divider></el-divider>
+                <span>Body: {{selectedPost.body}}</span>
+              </div>
+            </el-card>
+          </div>
+        </el-drawer>
+      </div>
+    </el-drawer>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "BlogManager",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      drawer: false,
+      innerDrawer: false,
+      users: null,
+      selectedUser: null,
+      postsWrittenBySelectedUser: null,
+      selectedPost: null
+    };
+  },
+  methods: {
+    fetchUsers() {
+      axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then(response => (this.users = response.data));
+    },
+    fetchPostsWrittenBySelectedUser(selectedUser) {
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts")
+        .then(
+          response =>
+            (this.postsWrittenBySelectedUser = response.data.filter(
+              x => x.userId === selectedUser.id
+            ))
+        );
+    },
+    onUserSelected(row) {
+      this.selectedUser = row;
+      this.drawer = true;
+      this.fetchPostsWrittenBySelectedUser(this.selectedUser);
+    },
+    onPostSelected(row) {
+      this.selectedPost = row;
+      this.innerDrawer = true;
+    }
+  },
+  mounted() {
+    this.fetchUsers();
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
+import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/en';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from "./App.vue";
 
+Vue.use(ElementUI, { locale });
 Vue.config.productionTip = false;
 
 new Vue({
