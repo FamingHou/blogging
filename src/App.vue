26867b672da726b4bbdbb6d4e974be2a791a90ed
Faming Hou <faming.hou@gmail.com>
2019-08-20 12:07:00 +1200

    Feature/infrastructure (#1)
    * create an initial project by using Vue cli
    
    * change default name to BlogManager
    
    * install element-ui

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <BlogManager msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import BlogManager from "./components/BlogManager.vue";
+
+export default {
+  name: "app",
+  components: {
+    BlogManager
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
